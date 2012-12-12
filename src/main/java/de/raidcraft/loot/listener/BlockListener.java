2c03b1d10b5f52b1236aa96e714970a20a126bd0
Philip Urban <urban.philip@googlemail.com>
2012-12-12 22:00:49 +0100

    chests respawn after explosion

--- src/main/java/de/raidcraft/loot/listener/BlockListener.java
+++ src/main/java/de/raidcraft/loot/listener/BlockListener.java
@@ -1,10 +1,19 @@
 package de.raidcraft.loot.listener;
 
+import com.sk89q.commandbook.CommandBook;
 import de.raidcraft.loot.LootFactory;
 import de.raidcraft.loot.util.LootChat;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: Philip
@@ -19,6 +28,34 @@ public class BlockListener implements Listener {
         if (LootFactory.inst.getLootObject(event.getBlock()) != null) {
             event.setCancelled(true);
             LootChat.warn(event.getPlayer(), "Loot Objekte können nicht zerstört werden!");
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onExplosion(EntityExplodeEvent event) {
+        List<Block> lootBlocks = new ArrayList<>();
+        for(Block block : event.blockList()) {
+            if (LootFactory.inst.getLootObject(block) != null) {
+                lootBlocks.add(block);
+            }
+        }
+        if(lootBlocks.size() > 0) {
+            TNTPlacerTask task = new TNTPlacerTask(lootBlocks);
+            Bukkit.getScheduler().scheduleSyncDelayedTask((Plugin)CommandBook.inst(), task, 20*5);
+        }
+    }
+    
+    public class TNTPlacerTask implements Runnable {
+        List<Block> chestBlocks;
+        
+        public TNTPlacerTask(List<Block> chestBlocks) {
+            this.chestBlocks = chestBlocks;
+        }
+        @Override
+        public void run() {
+            for(Block block : chestBlocks) {
+                block.setType(Material.CHEST);
+            }
         }
     }
 }

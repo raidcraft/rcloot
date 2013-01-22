11a34f9b76ff5b86dcc50b52c5deb31b7b8646bd
Philip Urban <urban.philip@googlemail.com>
2013-01-22 21:53:13 +0100

    used new item serialization in raidcraft api

--- src/main/java/de/raidcraft/loot/database/tables/LootTableEntriesTable.java
+++ src/main/java/de/raidcraft/loot/database/tables/LootTableEntriesTable.java
@@ -7,20 +7,15 @@ import de.raidcraft.loot.database.LootDatabase;
 import de.raidcraft.loot.table.LootTable;
 import de.raidcraft.loot.table.LootTableEntry;
 import de.raidcraft.loot.table.SimpleLootTableEntry;
-import org.bukkit.Color;
-import org.bukkit.FireworkEffect;
+import de.raidcraft.util.ItemUtils.ItemUtils;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.FireworkMeta;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Author: Philip
@@ -57,7 +52,8 @@ public class LootTableEntriesTable extends Table {
     public void addEntries(LootTable table) {
 
         for (LootTableEntry entry : table.getEntries()) {
-            String itemData = getItemData(entry.getItem());
+            ItemUtils.Serialization serialization = new ItemUtils.Serialization(entry.getItem());
+            String itemData = serialization.getSerializedItemData();
             // save entry if doesn't save yet
             if (entry.getId() == 0) {
                 try {
@@ -99,7 +95,8 @@ public class LootTableEntriesTable extends Table {
                         resultSet.getShort("durability")
                 );
 
-                addItemData(itemStack, resultSet.getString("itemdata"));
+                ItemUtils.Serialization serialization = new ItemUtils.Serialization(itemStack);
+                itemStack = serialization.getDeserializedItem(resultSet.getString("itemdata"));
 
                 SimpleLootTableEntry entry = new SimpleLootTableEntry();
                 entry.setId(resultSet.getInt("id"));
@@ -120,133 +117,6 @@ public class LootTableEntriesTable extends Table {
                     "DELETE FROM " + getTableName() + " WHERE loot_table_id = '" + table.getId() + "';").execute();
         } catch (SQLException e) {
             CommandBook.logger().warning(e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    private String getItemData(ItemStack item) {
-        if(item.getType() == Material.FIREWORK) {
-            return getFireworkEffectString(((FireworkMeta)item.getItemMeta()));
-        }
-        else if(item.getType() == Material.BOOK_AND_QUILL) {
-            //TODO save written books
-            return "";
-        }
-        else {
-            return getEnchantmentString(item.getItemMeta().getEnchants());
-        }
-    }
-    
-    private ItemStack addItemData(ItemStack itemstack, String data) {
-        if(itemstack.getType() == Material.FIREWORK) {
-            addFireworkEffectMetaByString(data, itemstack);
-        }
-        else if(itemstack.getType() == Material.BOOK_AND_QUILL) {
-            //TODO add book data
-        }
-        else {
-            itemstack.addEnchantments(getEnchantmentsByString(data));
-        }
-        return itemstack;
-    }
-    
-    /*
-     * Enchantment conversation methods
-     */
-    private String getEnchantmentString(Map<Enchantment, Integer> enchantments) {
-
-        String enchantmentString = "";
-        for (Map.Entry<Enchantment, Integer> enchantment : enchantments.entrySet()) {
-            enchantmentString += enchantment.getKey().getName() + ":" + enchantment.getValue() + "|";
-        }
-        return enchantmentString;
-    }
-
-    private Map<Enchantment, Integer> getEnchantmentsByString(String enchantmentString) {
-
-        Map<Enchantment, Integer> enchantments = new HashMap<>();
-        String[] enchantmentPairs = enchantmentString.split("\\|");
-        for (String enchantmentPair : enchantmentPairs) {
-            String[] enchantmentPairSplit = enchantmentPair.split(":");
-            try {
-                enchantments.put(Enchantment.getByName(enchantmentPairSplit[0]), Integer.parseInt(enchantmentPairSplit[1]));
-            } catch (Exception e) {
-            }
-        }
-        return enchantments;
-    }
-    
-    /*
-     * Firework conversation methods
-     * 
-     * FireworkEffects structure: 
-     * Power=EffectName:Flicker:Trail:Color1,Color2,...:FadeColor1,FadeColor2,...|EffectName2:...
-     */
-    private String getFireworkEffectString(FireworkMeta fireworkMeta) {
-        
-        String fireworkEffects = fireworkMeta.getPower() + "=";
-        for (FireworkEffect effect : fireworkMeta.getEffects()) {
-            fireworkEffects += effect.getType().name() + ":";
-            fireworkEffects += ((effect.hasFlicker()) ? "1" : "0") + ":";
-            fireworkEffects += ((effect.hasTrail()) ? "1" : "0") + ":";
-            for(Color color : effect.getColors()) {
-                fireworkEffects += color.asRGB() + ",";
-            }
-            fireworkEffects += ":";
-            for(Color color : effect.getFadeColors()) {
-                fireworkEffects += color.asRGB() + ",";
-            }
-            fireworkEffects += "|";
-        }
-        return fireworkEffects;
-    }
-    
-    private void addFireworkEffectMetaByString(String fireworkEffectMetaString, ItemStack fireworkItem) {
-        
-        try {
-            FireworkMeta fireworkMeta = (FireworkMeta)fireworkItem.getItemMeta();
-            String[] powerPair = fireworkEffectMetaString.split("=");
-
-            fireworkMeta.setPower(Integer.valueOf(powerPair[0]));
-            
-            String[] effects = powerPair[1].split("\\|");
-            
-            for(String effect : effects) {
-                String[] effectParameter = effect.split(":");
-                if(effectParameter.length < 4) continue;
-
-                boolean flicker = (effectParameter[1] == "1") ? true : false;
-                boolean trail = (effectParameter[2] == "1") ? true : false;
-                
-                String[] colorString = effectParameter[3].split(",");
-                List<Color> colors = new ArrayList<>();
-                for(String color : colorString) {
-                    if(color.length() <= 0) continue;
-                    colors.add(Color.fromRGB(Integer.valueOf(color)));
-                }
-
-                List<Color> fadeColors = new ArrayList<>();
-                if(effectParameter.length > 4) {
-                    String[] fadeColorString = effectParameter[4].split(",");
-                    for(String color : fadeColorString) {
-                        if(color.length() <= 0) continue;
-                        fadeColors.add(Color.fromRGB(Integer.valueOf(color)));
-                    }
-                }
-                
-                FireworkEffect fireworkEffect = FireworkEffect.builder()
-                        .with(FireworkEffect.Type.valueOf(effectParameter[0]))
-                        .flicker(flicker)
-                        .trail(trail)
-                        .withColor(colors)
-                        .withFade(fadeColors)
-                        .build();
-
-                fireworkMeta.addEffect(fireworkEffect);
-            }
-            fireworkItem.setItemMeta(fireworkMeta);
-        }
-        catch(Exception e) {
             e.printStackTrace();
         }
     }
